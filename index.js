e8416bf79d5c4a9d959a2a6d0142e617fad5d458
PJ Frias <peejfrias@gmail.com>
2017-11-06 02:41:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+const HQ = 42;
+
+function distanceFromHqInBlocks(location) {
+  let distance = Math.abs(location-HQ);
+  return distance
+}
+
+function distanceFromHqInFeet(location) {
+  let distance = Math.abs(location-HQ);
+  return distance*264
+}
